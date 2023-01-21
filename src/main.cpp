d876a6a55420e843cd1989631fb48be4f9859898
parthishere <parthishere1234@gmail.com>
2023-01-22 00:01:13 +0530

    code done

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,6 @@ bool initial = true;
 
 BluetoothSerial SerialBT;
 
-struct CardData
-{
-  byte rssi;
-  byte epc[12];
-};
-
 void setup()
 {
   // put your setup code here, to run once:
@@ -21,7 +15,6 @@ void setup()
   Serial.begin(115200);
   Serial2.begin(115200);
   delay(1000);
-  // Serial1.begin(9600, SERIAL_8N1, 4, 2); // RX, TX
   SerialBT.begin();
   Serial.println("Bluetooth Started! Ready to pair...");
 }
@@ -53,53 +46,56 @@ void loop()
 
       if (totalpackets == 0)
       {
-
-        initial = true;
-        for (int i = 0; i < count_t / 2; i++)
+        try
         {
-          byte temp = tdata[i];
-          tdata[i] = tdata[count_t - i - 1];
-          tdata[count_t - i - 1] = temp;
-        }
-
-        for (int i = 0; i < count_t; i++)
-        {
-          Serial.printf("%02X ", tdata[i]);
-        }
-
-        int num_cards = tdata[4];
-        Serial.printf("\nnum of cards: %d", num_cards);
-        // RSSI
-
-        // CardData cardData[num_cards];
-        byte rssi[num_cards];
-
-        for (int i = 1; i <= num_cards; i++)
-        {
-          int index = (13 * (i - 1)) + 5;
-          // cardData[i].rssi = tdata[index];
-          rssi[i - 1] = tdata[index];
-          Serial.printf("\nRSSI of card %d is %d", i, rssi[i - 1]);
-        }
-
-        // EPC
-        byte epc[10][12];
-        Serial.printf("\nData of Cards:");
-        for (int i = 0; i < num_cards; i++)
-        {
-          for (int j = 0; j < 12; j++)
+          initial = true;
+          for (int i = 0; i < count_t / 2; i++)
           {
-            int index = (13 * (i)) + 7 + j;
-            epc[i][j] = tdata[index];
-            // cardData[i].epc[j] = tdata[index];
-            Serial.printf("%02X ", epc[i][j]);
+            byte temp = tdata[i];
+            tdata[i] = tdata[count_t - i - 1];
+            tdata[count_t - i - 1] = temp;
+          }
+
+          for (int i = 0; i < count_t; i++)
+          {
+            Serial.printf("%02X ", tdata[i]);
+          }
+
+          int num_cards = tdata[4];
+          Serial.printf("\nnum of cards: %d", num_cards);
+          // RSSI
+
+          byte rssi[num_cards];
+
+          for (int i = 1; i <= num_cards; i++)
+          {
+            int index = (13 * (i - 1)) + 5;
+            rssi[i - 1] = tdata[index];
+            Serial.printf("\nRSSI of card %d is %d", i, rssi[i - 1]);
+          }
+
+          // EPC
+          byte epc[10][12];
+          Serial.printf("\nData of Cards:");
+          for (int i = 0; i < num_cards; i++)
+          {
+            for (int j = 0; j < 12; j++)
+            {
+              int index = (13 * (i)) + 7 + j;
+              epc[i][j] = tdata[index];
+              Serial.printf("%02X ", epc[i][j]);
+            }
+
+            Serial.printf("\n");
           }
 
           Serial.printf("\n");
+          break;
         }
-
-        Serial.printf("\n");
-        break;
+        catch (char *excp)
+        {
+          Serial.print("Error");
+        }
       }
     }
     // do the task that is related to variable;
