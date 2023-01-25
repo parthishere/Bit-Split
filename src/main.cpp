3698edfab9bc7164ebdad4171e449cb7a420bf79
parthishere <parthishere1234@gmail.com>
2023-01-26 01:28:16 +0530

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,36 @@
 
 int buzPin = 32, potPin = 4, b1pin = 27, b2pin = 26, b3pin = 25, b4pin = 33;
 
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+
+#define BLACK 0x0000
+#define BLUE 0x001F
+#define RED 0xF800
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
+#define PINK 0xEB34
+#define LIGHT_PINK 0x92f53b
+#define LIGHT_PURPLE 0x865b94
+#define LIGHT_BLUE 0x9933ff
+#define CYANN 0xE536F3
+
+const char message[] = "card detail";
+int charge_length = 50, is_charging;
+
+void inputs(char message[], int number, int strength = 0);
+void wifi(int x, int y, int number);
+void batteryDraw(int percentage, bool charge, int charging_percent);
+void modee();
+void charge();
+
+void change_delay(int intensity);
+
 int count = 0, count_t = 0, totalpackets = 0;
 byte tdata[200];
 bool initial = true;
@@ -13,8 +43,6 @@ const int ledChannel = 0;
 const int resolution = 8;
 
 long int last_millis;
-
-void change_delay(int intensity);
 
 BluetoothSerial SerialBT;
 
@@ -214,4 +242,107 @@ int calculate_distance(int rssi)
 {
 
   return 0;
+}
+
+void inputs(char message[], int number, int strength = 0) // number = number of box we want to print at screen , strength = signal strength of wifi
+{
+  tft.fillRect(5, 90 + number * 30, 220, 25, BLACK);
+  tft.drawRect(5, 90 + number * 30, 220, 25, LIGHT_PINK); // to print UID string rect 1
+  tft.setCursor(10, 95 + number * 30);
+  tft.setTextColor(CYAN);
+  tft.setTextSize(2.5);
+  tft.print(message);
+
+  Serial.println("Strrngth");
+  Serial.println(strength);
+  switch (strength)
+  {
+  case 1: // to create 1 bar of wifi signal
+    wifi(0, 0, number);
+    break;
+  case 2: // to create 2 bars
+    wifi(0, 0, number);
+    wifi(1, 1, number);
+    break;
+  case 3: // to create 3 bars
+    wifi(0, 0, number);
+    wifi(1, 1, number);
+    wifi(2, 2, number);
+    break;
+  case 4: // to create 4 bars
+    wifi(0, 0, number);
+    wifi(1, 1, number);
+    wifi(2, 2, number);
+    wifi(3, 3, number);
+    break;
+  default:
+    wifi(0, 0, number);
+    break;
+  }
+}
+
+void wifi(int x, int y, int number)
+{
+
+  tft.drawRect(180 + 10 * x, 104 - 4 * y + number * 30, 8, 8 + 4 * x, BLACK);
+  tft.fillRect(180 + 10 * x, 104 - 4 * y + number * 30, 8, 8 + 4 * x, BLACK);
+
+  tft.drawRect(180 + 10 * x, 104 - 4 * y + number * 30, 8, 8 + 4 * x, WHITE);
+  tft.fillRect(180 + 10 * x, 104 - 4 * y + number * 30, 8, 8 + 4 * x, WHITE);
+}
+
+void batteryDraw(int percentage, bool charge, int charging_percent)
+{
+  if (charge_length < 0)
+  {
+    charge_length = 50;
+  }
+  if (charge)
+  {
+    tft.drawRect(190, 0, 50, 30, WHITE);
+    tft.fillRect(190, 2, 50, 25, BLACK);
+    tft.fillRect(190 + charge_length, 2, 50, 25, GREEN);
+    tft.drawRect(180, 9, 10, 10, WHITE);
+    tft.fillRect(180, 9, 10, 10, WHITE);
+    charge_length -= 10;
+
+    Serial.println(charge_length);
+  }
+  else
+  {
+    Serial.println("charging process is not going on ");
+    int drawValue = map(percentage, 0, 100, 0, 50);
+    int battery_x = 190 + (50 - (percentage * 50 / 100));
+
+    if (percentage <= 25)
+    {
+      tft.drawRect(190, 0, 50, 30, WHITE);
+      tft.fillRect(battery_x, 2, drawValue, 25, RED);
+      tft.drawRect(180, 9, 10, 10, WHITE);
+      tft.fillRect(180, 9, 10, 10, WHITE);
+    }
+    else if (percentage == 100)
+    {
+      tft.drawRect(190, 0, 50, 30, WHITE);
+      tft.fillRect(battery_x, 2, drawValue, 25, PINK);
+      tft.drawRect(180, 9, 10, 10, WHITE);
+      tft.fillRect(180, 9, 10, 10, WHITE);
+    }
+    else
+    {
+      tft.drawRect(190, 0, 50, 30, WHITE);
+      tft.fillRect(battery_x, 2, drawValue, 25, GREEN);
+      tft.drawRect(180, 9, 10, 10, WHITE);
+      tft.fillRect(180, 9, 10, 10, WHITE);
+    }
+  }
+}
+
+void modee()
+{
+  tft.setCursor(0, 10); // to write title "mode"
+  tft.setTextColor(LIGHT_PINK);
+  tft.setTextSize(2.8);
+  tft.print("MODE");
+  tft.drawRect(4, 34, 225, 40, MAGENTA);
 }
